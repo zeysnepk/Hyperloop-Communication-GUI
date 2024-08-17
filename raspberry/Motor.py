38517d969201d99ee0adf9a3c4ecf23a2d8be53a
Zeynep <146364601+zeysnepk@users.noreply.github.com>
2024-08-17 22:05:10 +0300

    Update Motor.py

--- raspberry/Motor.py
+++ raspberry/Motor.py
@@ -14,22 +14,43 @@ GPIO.output(in1, GPIO.LOW)
 GPIO.output(in2, GPIO.LOW)
 p = GPIO.PWM(en, 1000)
 
-p.start(0)  # PWM sinyalini başlangıçta 0 olarak ayarla, motoru tamamen durdur
+p.start(0)  #  Initially set the PWM signal to 0, stop the motor completely
 
-def run_motor():
-        GPIO.output(in1, GPIO.HIGH)
-        GPIO.output(in2, GPIO.LOW)
-    
-def stop_motor():
-        GPIO.output(in1, GPIO.LOW)
-        GPIO.output(in2, GPIO.LOW)
+def run(command):
+        if command == 'start':
+                GPIO.output(in1, GPIO.HIGH)
+                GPIO.output(in2, GPIO.LOW)
+                en.ChangeDutyCycle(50)  # %50 speed
+        elif command == 'stop':
+                GPIO.output(in1, GPIO.LOW)
+                GPIO.output(in2, GPIO.LOW)
+                en.ChangeDutyCycle(0)  # stop the motor
+        elif command == 'backward':
+                GPIO.output(in1, GPIO.LOW)
+                GPIO.output(in2, GPIO.HIGH)
+                en.ChangeDutyCycle(50)  # %50 speed
+        elif command == 'forward':
+                GPIO.output(in1, GPIO.HIGH)
+                GPIO.output(in2, GPIO.LOW)
+                en.ChangeDutyCycle(50)  # %50 speed
     
 
 def motor_control(command):
-    if command == 'D':
-        run_motor()   # Servo motoru 90 dereceye getir
-        time.sleep(1)         # 5 saniye bekle
-        stop_motor()    # Servo motoru başlangıç pozisyonuna getir
+        if command == 'D':
+                run('stop')
+                time.sleep(2)
+        elif command == 'B':
+                run('start')
+                time.sleep(2)
+        elif command == 'F':
+                run('forward')
+                time.sleep(2)
+        elif command == 'A':
+                run('backward')
+                time.sleep(2)
+        else:
+                print("Please press a buton")
         
 def cleanup():
-    GPIO.cleanup()  # GPIO pinlerini serbest bırak
+        p.stop()
+        GPIO.cleanup() 
